c9510cafef71b95d9a01168f1d26a5a87ed45a8e
Barasa-Micah <barasamica@gmail.com>
2024-07-06 16:57:33 +0300

    Styled the entire application

---
+++ Styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+});
